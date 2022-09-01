c5738cc3d85a32a602c26e017ad92911343c831b
thepicture <85333076+thepicture@users.noreply.github.com>
2022-09-01 17:53:56 +0500

    feat: add splash screen with logo

--- KazanNeft.MobileApp/KazanNeft.MobileApp.Android/MainActivity.cs
+++ KazanNeft.MobileApp/KazanNeft.MobileApp.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using AndroidX.AppCompat.App;
 
 namespace KazanNeft.MobileApp.Droid
 {
-    [Activity(Label = "KazanNeft.MobileApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "KazanNeft.MobileApp", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
