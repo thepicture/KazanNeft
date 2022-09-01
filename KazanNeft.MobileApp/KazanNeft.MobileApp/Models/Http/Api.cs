dda0ec3bb4f4117ecf4ca4f2a0954c2be9140ca6
thepicture <85333076+thepicture@users.noreply.github.com>
2022-09-01 21:10:19 +0500

    feat(AssetCataloguePages): add connection to api

--- KazanNeft.MobileApp/KazanNeft.MobileApp/Models/Http/Api.cs
+++ KazanNeft.MobileApp/KazanNeft.MobileApp/Models/Http/Api.cs
@@ -2,7 +2,7 @@
 {
     public class Api
     {
-        public static string BaseUrl = "http://10.0.2.2:58989/api";
+        public static string BaseUrl = "http://10.0.2.2:3000/api";
         public static string AssetsUrl = BaseUrl + "/assets";
     }
 }
--- KazanNeft.MobileApp/KazanNeft.MobileApp/Pages/AssetCataloguePages.xaml.cs
+++ KazanNeft.MobileApp/KazanNeft.MobileApp/Pages/AssetCataloguePages.xaml.cs
@@ -1,10 +1,7 @@
-﻿
-using KazanNeft.MobileApp.Models.Entities;
+﻿using KazanNeft.MobileApp.Models.Entities;
 using Newtonsoft.Json;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,24 +10,22 @@ namespace KazanNeft.MobileApp.Forms
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AssetCataloguePage : ContentPage
     {
-        public readonly string artificialResponse = @"[{""ID"":1,""AssetName"":""Toyota Hilux FAF321"",""AssetSN"":""05/04/0001"",""DepartmentName"":""Distribution"",""AssetGroupName"":""Mechanical "",""WarrantyDate"":null},{""ID"":2,""AssetName"":""Suction Line 852"",""AssetSN"":""04/03/0001"",""DepartmentName"":""R&D"",""AssetGroupName"":""Electrical"",""WarrantyDate"":""2020-03-02T00:00:00""},{""ID"":3,""AssetName"":""ZENY 3,5CFM Single-Stage 5 Pa Rotary Vane"",""AssetSN"":""01/01/0001"",""DepartmentName"":""Exploration"",""AssetGroupName"":""Hydraulic"",""WarrantyDate"":""2018-01-17T00:00:00""},{""ID"":4,""AssetName"":""Volvo FH16"",""AssetSN"":""05/04/0002"",""DepartmentName"":""Distribution"",""AssetGroupName"":""Mechanical "",""WarrantyDate"":null}]";
-
         public AssetCataloguePage()
         {
             InitializeComponent();
             var departments = new List<Department>
             {
-                new Department {Name = nameof(Department)},
+                new Department { Name = nameof(Department) },
             };
             var assetGroups = new List<AssetGroup>
             {
-                new AssetGroup {Name = nameof(AssetGroup).Replace("tG", "t G")},
+                new AssetGroup { Name = "Asset Group" },
             };
-            BindableLayout.SetItemsSource(AssetList,
-                JsonConvert.DeserializeObject<Asset[]>(artificialResponse));
-            AssetCount.Text = BindableLayout.GetItemsSource(AssetList).Cast<Asset>().Count()
-                + " assets from "
-                + BindableLayout.GetItemsSource(AssetList).Cast<Asset>().Count();
+            WebClient client = new WebClient { Encoding = System.Text.Encoding.UTF8 };
+            string response = client.DownloadString(Api.AssetsUrl);
+            Asset[] assets = JsonConvert.DeserializeObject<Asset[]>(response);
+            BindableLayout.SetItemsSource(AssetList, assets);
+            AssetCount.Text = assets.Length + " assets from " + assets.Length;
             DepartmentPicker.ItemsSource = departments;
             AssetGroupPicker.ItemsSource = assetGroups;
             DepartmentPicker.SelectedIndex = 0;
